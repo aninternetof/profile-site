e96dd4336327b015829f1d75e66a643b85ce6618
unknown <blhurlburt@010309IND.indesign.net>
2015-04-19 17:24:19 -0400

    [index] added featured projects slide

--- start_web_server.bat
+++ start_web_server.bat
@@ -1,4 +1,4 @@
-cd C:\Users\blhurlburt\Personal_Projects\aninternetof
+cd C:\Users\blhurlburt\Personal_Projects\aninternetof\profile-site\
 echo "Starting server..."
 C:\Python27\python.exe -m SimpleHTTPServer 81
 echo "... server started."
