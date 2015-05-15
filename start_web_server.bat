21fc66958dc852dee3740369cab95687f3a0ea46
unknown <blhurlburt@010309IND.indesign.net>
2015-05-15 16:37:35 -0400

    [content] Expanded and restructured about me

--- start_web_server.bat
+++ start_web_server.bat
@@ -1,4 +1,4 @@
-cd C:\Users\blhurlburt\Personal_Projects\aninternetof\profile-site\
+cd C:\Users\blhurlburt\Personal_Projects\profile-site\
 echo "Starting server..."
 C:\Python27\python.exe -m SimpleHTTPServer 81
 echo "... server started."
